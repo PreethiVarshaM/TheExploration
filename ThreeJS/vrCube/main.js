2bc4f4f2d683861bc54f7d100175c2062c760cb4
Preethi Varsha <mpreethivarsha@gmail.com>
2022-06-29 20:48:24 +0530

    update

--- ThreeJS/vrCube/main.js
+++ ThreeJS/vrCube/main.js
@@ -1,33 +1,53 @@
 import './style.css'
-import * as Three from 'three';
+import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
-let scene = new Three.Scene()
-let camera = new Three.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 45, 30000)
-let renderer = new Three.WebGLRenderer({ antilias: true });
+
+let scene, camera, renderer
+
+function animate() {
+    renderer.render(scene, camera)
+    requestAnimationFrame(animate)
+}
 
 function init() {
-  camera.position.set(-900, -200, -900)
-  renderer.setSize(window.innerWidth, window.innerHeight)
-  document.body.appendChild(renderer.domElement)
+    scene = new THREE.Scene()
+    camera = new THREE.PerspectiveCamera(55, window.innerWidth / window.innerHeight, 45, 30000)
+    renderer = new THREE.WebGLRenderer({ antialias: true });
 
-  let controls = new Three.OrbitControls(camera)
-  controls.addEventListener('change', renderer)
+    camera.position.set(-900, -200, -900)
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    document.body.appendChild(renderer.domElement)
 
-  let materialArray = []
-  let ft = new Three.TextureLoader().load('ThreeJS/penguins-skybox-pack/penguins9/divine_ft')
-  let bk = new Three.TextureLoader().load('ThreeJS/penguins-skybox-pack/penguins9/divine_bk')
-  let up = new Three.TextureLoader().load('ThreeJS/penguins-skybox-pack/penguins9/divine_up')
-  let dn = new Three.TextureLoader().load('ThreeJS/penguins-skybox-pack/penguins9/divine_dn')
-  let rt = new Three.TextureLoader().load('ThreeJS/penguins-skybox-pack/penguins9/divine_rt')
-  let lf = new Three.TextureLoader().load('ThreeJS/penguins-skybox-pack/penguins9/divine_lf')
-  materialArray.push(new Three.MeshBasicMaterial({ map: ft }))
-  materialArray.push(new Three.MeshBasicMaterial({ map: bk }))
-  materialArray.push(new Three.MeshBasicMaterial({ map: up }))
-  materialArray.push(new Three.MeshBasicMaterial({ map: dn }))
-  materialArray.push(new Three.MeshBasicMaterial({ map: rt }))
-  materialArray.push(new Three.MeshBasicMaterial({ map: lf }))
+    let controls = new OrbitControls(camera)
+    controls.addEventListener('change', renderer)
+    controls.minDistance = 500
+    controls.maxDistance = 1500
 
-  let skyBoxGeometry = new Three.BoxGeometry(10000, 10000, 10000)
+    let materialArray = []
+    let ft = new THREE.TextureLoader().load('divine_ft.jpg')
+    let bk = new THREE.TextureLoader().load('divine_bk.jpg')
+    let up = new THREE.TextureLoader().load('divine_up.jpg')
+    let dn = new THREE.TextureLoader().load('divine_dn.jpg')
+    let rt = new THREE.TextureLoader().load('divine_rt.jpg')
+    let lf = new THREE.TextureLoader().load('divine_lf.jpg')
+    materialArray.push(new THREE.MeshBasicMaterial({ map: ft }))
+    materialArray.push(new THREE.MeshBasicMaterial({ map: bk }))
+    materialArray.push(new THREE.MeshBasicMaterial({ map: up }))
+    materialArray.push(new THREE.MeshBasicMaterial({ map: dn }))
+    materialArray.push(new THREE.MeshBasicMaterial({ map: rt }))
+    materialArray.push(new THREE.MeshBasicMaterial({ map: lf }))
+
+    for (let i = 0; i < 6; i++) {
+        materialArray[i].side = THREE.BackSide;
+    }
+
+    let skyBoxGeometry = new THREE.BoxGeometry(10000, 10000, 10000)
+    let skybox = new THREE.Mesh(skyBoxGeometry, materialArray)
+    scene.add(skybox)
+    animate();
 }
-init()
+
+init();
+
+
